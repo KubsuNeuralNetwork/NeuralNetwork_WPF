f061d733c872b3ec5ae99444528ff80e1e865ede
Storchak_VV <vadim.storchak@utair.ru>
2021-04-20 09:47:33 +0300

    Learning_sample

--- WPF_Main/MainWindow.xaml.cs
+++ WPF_Main/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Main.Components.Models;
 
 namespace WPF_Main
 {
@@ -23,6 +26,22 @@ namespace WPF_Main
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) {
+                try
+                {
+                    Learning_sample ls = new Learning_sample(File.ReadAllText(openFileDialog.FileName));
+                    File_text.Text = ls.Learning_sample_str;
+                } catch (System.ArgumentException ex)
+                {
+                    File_text.Text = "Файл имена солбцов содержат одинаковые имена";
+                    File_text.Foreground = Brushes.Red;
+                }
+            }
         }
     }
 }
