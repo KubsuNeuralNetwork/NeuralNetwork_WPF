be35369424250c5dd909cf9520cd771c7dafa0a6
QbGogin <61201459+QbGogin@users.noreply.github.com>
2021-05-18 12:37:12 +0300

    Add files via upload
    Нормализация

--- WPF_Main/Components/Service/Manager.cs
+++ WPF_Main/Components/Service/Manager.cs
@@ -7,21 +7,22 @@ public class Manager
     static int[] layers = new int[3] { 3, 5, 1 };
     static string[] activation = new string[2] { "leakyrelu", "leakyrelu" };
 
-    public static void Main()
+    public static void Run()
     {
         net = new NeuralNetwork(layers, activation);
-        //for (int i = 0; i < 20000; i++)
-        //{
-        //    net.BackPropagate(new float[] { 0, 0, 0 }, new float[] { 0 });
-        //    net.BackPropagate(new float[] { 1, 0, 0 }, new float[] { 1 });
-        //    net.BackPropagate(new float[] { 0, 1, 0 }, new float[] { 1 });
-        //    net.BackPropagate(new float[] { 0, 0, 1 }, new float[] { 1 });
-        //    net.BackPropagate(new float[] { 1, 1, 0 }, new float[] { 1 });
-        //    net.BackPropagate(new float[] { 0, 1, 1 }, new float[] { 1 });
-        //    net.BackPropagate(new float[] { 1, 0, 1 }, new float[] { 1 });
-        //    net.BackPropagate(new float[] { 1, 1, 1 }, new float[] { 1 });
-        //}
-        net.Load("./MLP.txt");
+        
+        for (int i = 0; i < 20000; i++)
+        {
+            net.BackPropagate(new float[] { 0, 0, 0 }, new float[] { 0 });
+            net.BackPropagate(new float[] { 1, 0, 0 }, new float[] { 1 });
+            net.BackPropagate(new float[] { 0, 1, 0 }, new float[] { 1 });
+            net.BackPropagate(new float[] { 0, 0, 1 }, new float[] { 1 });
+            net.BackPropagate(new float[] { 1, 1, 0 }, new float[] { 1 });
+            net.BackPropagate(new float[] { 0, 1, 1 }, new float[] { 1 });
+            net.BackPropagate(new float[] { 1, 0, 1 }, new float[] { 1 });
+            net.BackPropagate(new float[] { 1, 1, 1 }, new float[] { 1 });
+        }
+        // net.Load("./MLP.txt");
         Console.WriteLine("cost: " + net.cost);
 
         Console.WriteLine(net.FeedForward(new float[] { 0, 0, 0 })[0]);
